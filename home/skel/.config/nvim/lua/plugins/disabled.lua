1a705544e30dcf7127f71d5f14e9706500ec726d
me <rosllarr@tutanota.com>
2023-11-30 13:35:54 +0700

    install lazyvim

---
+++ home/skel/.config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "folke/trouble.nvim", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+  { "mfussenegger/nvim-lint", enabled = false },
+  { "stevearc/dressing.nvim", enabled = false },
+}
